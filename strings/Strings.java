fede6cdcea238fd05109c67bc664d3a431d300e1
Himanshu Rajpoot <himanshurajpoot@Himanshus-MacBook-Air.local>
2026-05-12 14:35:59 +0530

    completed

--- strings/Strings.java
+++ strings/Strings.java
@@ -44,7 +44,7 @@ public class Strings{
 
 
 
-    // Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
+    //    Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
     // Example : 
     // email = “apnaCollegeJava@gmail.com” ; username = “apnaCollegeJava” 
     // email = “helloWorld123@gmail.com”; username = “helloWorld123”
