d7b9df7a7779ea121da42ff3a3729ae1ceb00ae9
Himanshu Rajpoot <himanshurajpoot@Himanshus-MacBook-Air.local>
2026-06-26 06:01:23 +0530

    completed

---
+++ sorting/Sorting.java
@@ -0,0 +1,92 @@
+import java.util.Arrays;
+
+public class Sorting{
+    // merge sort
+    // public static void concure(int arr[],int si, int ei, int mid){
+    //     int marged[] = new int[ei-si+1];
+
+    //     int ind1= si;
+    //     int ind2 = mid+1;
+    //     int x =0;
+
+    //     // 
+    //     while (ind1<=mid&&ind2<=ei) {
+    //         if(arr[ind1]<=arr[ind2]){
+    //             marged[x++]=arr[ind1++];
+    //         }else{
+    //             marged[x++]=arr[ind2++];
+    //         }
+    //     }
+
+    //     while (ind1<=mid) {
+    //          marged[x++]=arr[ind1++];
+    //     }
+
+    //     while (ind2<=ei) {
+    //          marged[x++]=arr[ind2++];
+    //     }
+
+    //     for(int i =0,j=si;i<marged.length;i++,j++){
+    //         arr[j]=marged[i];
+    //     }
+    // }
+    // public static void divide(int arr[],int si, int ei){
+    //      if(si>=ei){
+    //         return;
+    //      }
+
+    //      int mid = si +(ei-si)/2;
+    //      divide(arr, si, mid);
+    //      divide(arr, mid+1, ei);
+    //      concure(arr,si,ei,mid);
+    // }
+    // public static void main(String arge[]){
+    //     int arr[] = {3,2,5,6,4,88,44};
+    //     int n = arr.length;
+     
+    //     divide(arr,0,n-1);
+    //     System.out.println(Arrays.toString(arr));
+    // }
+
+    // Quick Sort
+
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        i++;
+        int temp=arr[i];
+        arr[i]=pivot;
+        arr[high]=temp;
+        return i; //pivot index
+
+    }
+
+    public static void quickSort(int arr[], int low, int high){
+        if(low<high){
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, high-1);
+            quickSort(arr, pi+1, high);
+
+        }
+
+    }
+
+    public static void main(String arge[]){
+        int arr[] = {2,3,4,1,66,22,33,78,34};
+        int n = arr.length;
+        quickSort(arr,0,n-1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
